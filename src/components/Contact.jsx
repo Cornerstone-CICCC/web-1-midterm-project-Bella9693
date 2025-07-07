166691e40b3de35230d08ad8db41c0696b7fa5c9
Bella9693 <realbella9693@gmail.com>
2025-07-07 14:34:35 -0700

    exercise

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,33 +5,37 @@ import github from "../assets/img/github-brands.svg";
 import linkedin from "../assets/img/linkedin-brands.svg";
 
 const Contact = () => {
-  <section id="contact">
-    <div className="contact__inner">
-      <h2 className="contact__title">Contact</h2>
-      <div className="contact__intro">
-        <p>Open to new opportunities! Feel free to connect and collaborate.</p>
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">Contact</h2>
+        <div className="contact__intro">
+          <p>
+            Open to new opportunities! Feel free to connect and collaborate.
+          </p>
+        </div>
+        <div className="contact__img">
+          <ul>
+            <li>
+              <div>
+                <img src={envelope} alt="mail" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <img src={github} alt="github" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <img src={linkedin} alt="linkedin" />
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="contact__img">
-        <ul>
-          <li>
-            <div>
-              <img src={envelope} alt="mail" />
-            </div>
-          </li>
-          <li>
-            <div>
-              <img src={github} alt="github" />
-            </div>
-          </li>
-          <li>
-            <div>
-              <img src={linkedin} alt="linkedin" />
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default Contact;
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
 import Pro from "../components/Pro";
-import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Home from "../components/Home";
+import Contact from "../components/Contact";
 
 const HomeView = () => {
   return (
